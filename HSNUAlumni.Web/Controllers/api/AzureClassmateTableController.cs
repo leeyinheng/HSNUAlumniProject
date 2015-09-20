e2e96a4ea9bbbfec1d30664fb3c04e6129ce2177
Henry Lee <leeyinheng@yahoo.com>
2015-09-20 11:43:53 -0500

    v5
    v5

--- HSNUAlumni.Web/Controllers/api/AzureClassmateTableController.cs
+++ HSNUAlumni.Web/Controllers/api/AzureClassmateTableController.cs
@@ -5,7 +5,10 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using HSNUAlumni.ModelLib;
-using HSNUAlumni.DALLib; 
+using HSNUAlumni.DALLib;
+using System.Web;
+using System.Threading.Tasks;
+using System.Net.Http.Formatting;
 
 namespace HSNUAlumni.Web.Controllers.api
 {
@@ -31,10 +34,16 @@ namespace HSNUAlumni.Web.Controllers.api
         [HttpPost]
         public void AddOrUpdateClassmate(Classmate entity)
         {
-            entity.PartitionKey = entity.ClassId;
-            entity.RowKey = entity.Name; 
+            if (entity != null)
+            {
+                entity.PartitionKey = entity.ClassId;
 
-            op.AddorUpdateEntity(entity); 
+                entity.RowKey = entity.Name;
+
+                op.AddorUpdateEntity(entity);
+
+            }
+         
         }
 
         [Route("api/classmates/AddorUpdateList")]
@@ -49,6 +58,35 @@ namespace HSNUAlumni.Web.Controllers.api
         public void DeleteClassmate(string classId, string Name)
         {
             op.DeleteEntityByKeys(classId,Name);
+        }
+
+        [Route("api/classmate/uploadphoto")]
+        [HttpPost]
+        public async Task<string> asyncUploadPhoto()
+        {
+
+           
+            return "no file"; 
+
+            //if (!uploadedFile.Content.IsMimeMultipartContent()) return "no file";
+                                
+
+            //string filename = String.Format("{0:yyyyMMddHHmm}", DateTime.Now) + ".jpg";
+
+            //var filePath = HttpContext.Current.Server.MapPath("~/Image/");
+
+            //var streamProvider = new MultipartFormDataStreamProvider(filePath);
+
+            //await uploadedFile.Content.ReadAsMultipartAsync(streamProvider);
+
+            //foreach (var file in streamProvider.FileData)
+            //{
+            //    Console.WriteLine(file.Headers.ContentDisposition.FileName);
+            //    System.IO.File.Move(file.LocalFileName, filePath); 
+            //}
+
+            //return filename;
+
         }
     }
 }
