8368b86371dc694b808a1278acda94c497a0befc
Henry Lee <leeyinheng@yahoo.com>
2015-09-27 23:53:22 -0500

    v12
    v12

--- HSNUAlumni.Test/TestClassmateTable.cs
+++ HSNUAlumni.Test/TestClassmateTable.cs
@@ -8,55 +8,55 @@ namespace HSNUAlumni.Test
     [TestClass]
     public class TestClassmateTable
     {
-        [TestMethod]
-        public  void TestClassmateOperation()
-        {
-            var newClassmate = new Classmate("618", "Ken")
-            {
-                Gender = true,       
-                CellPhone = "901-990-0011", 
-                Status = ClassmateStatus.StayInTouch.ToString(), 
-                Notes = "la la lal al al la l l"
+        //[TestMethod]
+        //public  void TestClassmateOperation()
+        //{
+        //    var newClassmate = new Classmate("618", "Ken")
+        //    {
+        //        Gender = true,       
+        //        CellPhone = "901-990-0011", 
+        //        Status = ClassmateStatus.StayInTouch.ToString(), 
+        //        Notes = "la la lal al al la l l"
 
-            };
+        //    };
 
             
             
 
-           // newClassmate.CellPhone = "832-372-7188";
-           // newClassmate.Company = "Loomis";
-           //// newClassmate.Status = ClassmateStatus.StayInTouch;
-           // newClassmate.SeatNumber = 75;
+        //   // newClassmate.CellPhone = "832-372-7188";
+        //   // newClassmate.Company = "Loomis";
+        //   //// newClassmate.Status = ClassmateStatus.StayInTouch;
+        //   // newClassmate.SeatNumber = 75;
 
-            CustomerEntity customer = new CustomerEntity("333", "test123")
-            {
-                Email = "Walter@contoso.com",
-                PhoneNumber = "425-555-0101", 
-                Test = "test"
-            };
+        //    CustomerEntity customer = new CustomerEntity("333", "test123")
+        //    {
+        //        Email = "Walter@contoso.com",
+        //        PhoneNumber = "425-555-0101", 
+        //        Test = "test"
+        //    };
 
-            var op = new HSNUAlumni.DALLib.CloudTableOperation<HSNUAlumni.ModelLib.Classmate>("classmates");
+        //    var op = new HSNUAlumni.DALLib.CloudTableOperation<HSNUAlumni.ModelLib.Classmate>("classmates");
 
-            op.AddorUpdateEntity(newClassmate); 
+        //    op.AddorUpdateEntity(newClassmate); 
 
-        }
+        //}
 
 
-        [TestMethod]
-        public void TestInsertClassmateList()
-        {
-            var list = new List<ModelLib.Classmate>();
+        //[TestMethod]
+        //public void TestInsertClassmateList()
+        //{
+        //    var list = new List<ModelLib.Classmate>();
 
-            list.Add(new Classmate("618", "AAA") { Birthday = "08/09/1973", Gender = false });
-            list.Add(new Classmate("618", "BBB") { Birthday = "08/09/1973", Gender = false });
-            list.Add(new Classmate("618", "CCC") { Birthday = "08/09/1973", Gender = false });
-            list.Add(new Classmate("618", "DDD") { Birthday = "08/09/1973", Gender = false });
-            list.Add(new Classmate("618", "EEE") { Birthday = "08/09/1973", Gender = false });
+        //    list.Add(new Classmate("618", "AAA") { Birthday = "08/09/1973", Gender = false });
+        //    list.Add(new Classmate("618", "BBB") { Birthday = "08/09/1973", Gender = false });
+        //    list.Add(new Classmate("618", "CCC") { Birthday = "08/09/1973", Gender = false });
+        //    list.Add(new Classmate("618", "DDD") { Birthday = "08/09/1973", Gender = false });
+        //    list.Add(new Classmate("618", "EEE") { Birthday = "08/09/1973", Gender = false });
 
-            var op = new DALLib.CloudTableOperation<HSNUAlumni.ModelLib.Classmate>("classmates");
+        //    var op = new DALLib.CloudTableOperation<HSNUAlumni.ModelLib.Classmate>("classmates");
 
-            op.AddorUpdateEntityList(list); 
-        }
+        //    op.AddorUpdateEntityList(list); 
+        //}
 
        
 
